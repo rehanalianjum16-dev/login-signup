b469093b4e792c362e5c8ca169c6ea2da9504024
Rehan Ali Anjum <rehanalianjum16@gmail.com>
2026-08-05 22:48:15 +0500

    new

--- src/app/git/page.tsx
+++ src/app/git/page.tsx
@@ -6,9 +6,7 @@ function page() {
       <h1>hello word</h1>
       <h2>Rehan</h2>
       <h6>Abjum</h6>
-      <h6>Javed iqbal</h6>
-      <div>Assalam o Alaikum</div>
-      <h2>Rehan ali</h2>
+      <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero atque assumenda modi. Quaerat modi eligendi quam nobis repudiandae vero nulla ducimus totam amet.</h3>
       <h5>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque impedit, in sed optio architecto eligendi explicabo.</h5>
     </div>
   )
